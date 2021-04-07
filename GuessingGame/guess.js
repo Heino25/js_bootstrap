57ea753855ebddb848bbac653d1e588133b71b94
Heino25 <82093752+Heino25@users.noreply.github.com>
2021-04-07 13:28:49 +0200

    Add files via upload

---
+++ GuessingGame/guess.js
@@ -0,0 +1,31 @@
+var secret = Math.floor(Math.random() * 10) + 1;
+var active = true;
+
+function guessNumber() {
+    var guess = prompt("Please guess a number between 1 to 10.");
+    checkAnswer(guess);
+}
+
+function checkAnswer(guess) {
+    while (active) {
+        if (parseInt(guess) === secret) {
+            alert("Congratulations! your answer is correct.");
+            active = false;
+            reloadPage();
+        } else if (parseInt(guess) > secret) {
+            alert("Incorrect! Your guess is greater than the answer.");
+            guessNumber();
+        } else if (parseInt(guess) < secret) {
+            alert("Incorrect! Your answer is smaller than the answer.")
+            guessNumber();
+        } else {
+            alert("Invalid input! Please enter a number.");
+            guessNumber();
+        }
+    }
+}
+
+function reloadPage() {
+    window.location.reload();
+
+}
